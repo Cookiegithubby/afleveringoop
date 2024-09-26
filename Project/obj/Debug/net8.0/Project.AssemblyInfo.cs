5b6adab0dc6ae8bea7347c650bb15a309216f5ee
Cookiegithubby <vbf2003@gmail.com>
2024-09-26 10:08:50 +0200

    initial push

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c640ecaeecf7ec401a06c741f1ab8660595e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08db5997184e4549f392170413325b766f175934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
